93bf1a831aba1635f1c906faadc0608be911b992
Arjun P <arjun.parameswaran12@gmail.com>
2020-02-10 23:22:16 +0530

    first commit

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/lib/pq"
+	"github.com/subosito/gotenv"
 )
 
 type Book struct {
@@ -16,16 +20,43 @@ type Book struct {
 }
 
 var book []Book
+var db *sql.DB
+
+func init() {
+	gotenv.Load()
+}
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	fmt.Println("Libraray Application...")
+	fmt.Println("Library Application...")
+
+	pgUrl, err := pq.ParseURL(os.Getenv("ELEPHANTSQL_URL"))
+	logFatal(err)
+
+	db, err = sql.Open("postgres", pgUrl)
+	logFatal(err)
+
+	err = db.Ping()
+	logFatal(err)
+
+	log.Println(pgUrl)
+
 	router := mux.NewRouter()
 
-	router.HandleFunc("/books",getBooks).Methods("GET")
-	router.HandleFunc("/books/{id}",getBook).Methods("GET")
-	router.HandleFunc("/books",addBooks).Methods("POST")
-	router.HandleFunc("/books",updateBook).Methods("PUT")
-	router.HandleFunc("/books/{id}",removeBook).Methods("GET")
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	// router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	// router.HandleFunc("/books", addBooks).Methods("POST")
+	// router.HandleFunc("/books", updateBook).Methods("PUT")
+	// router.HandleFunc("/books/{id}", removeBook).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":8000",router))
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
 }
